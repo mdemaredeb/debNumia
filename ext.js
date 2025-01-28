4feb400f2d3bf4c058a0aa2f2eeece998f8b977d
Dema <34817965+m-demare@users.noreply.github.com>
2025-01-28 10:24:10 -0300

    Agrego redirección de wikijs

--- ext.js
+++ ext.js
@@ -1,6 +1,7 @@
 const redirects = {
     "debmedia.freshdesk.com": "numia.freshdesk.com",
     "support.debmedia.com": "support.numia.co",
+    "wikijs.debmedia.com": "wikijs.numia.co",
 };
 
 const redir = redirects[location.hostname];
